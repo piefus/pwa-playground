b52660af3605aba9f976cfdbaca574eace3a4772
Piero Fusacchia <p.fusacchia@wybe.it>
2021-01-15 18:34:46 +0100

    update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// self.importScripts('data/games.js');
+// Per aggiornare il sw ricordati di cambiare la versione del cacheName
 
 // Files to cache
 const cacheName = ["pwaPlayground-v12"];
@@ -9,44 +9,6 @@ const appShellFiles = ["index.html", "assets/main.js", "assets/style.css"];
 // }
 // const contentToCache = appShellFiles.concat(gamesImages);
 const contentToCache = appShellFiles;
-
-// Installing Service Worker
-// self.addEventListener("install", (e) => {
-//     console.log("[Service Worker] Install");
-//     e.waitUntil(
-//         (async () => {
-//             const cache = await caches.open(cacheName);
-//             console.log("[Service Worker] Caching all: app shell and content");
-//             await cache.addAll(contentToCache);
-//         })()
-//     );
-// });
-
-// Fetching content using Service Worker
-// self.addEventListener("fetch", (e) => {
-//     e.respondWith(
-//         (async () => {
-//             const r = await caches.match(e.request);
-//             console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-//             if (r) return r;
-//             const response = await fetch(e.request);
-//             const cache = await caches.open(cacheName);
-//             console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-//             cache.put(e.request, response.clone());
-//             return response;
-//         })()
-//     );
-// });
-
-// contentToCache.push("/pwa-playground/style.css");
-
-// self.addEventListener("install", (e) => {
-//     e.waitUntil(
-//         caches.open("pwaPlayground-v2").then((cache) => {
-//             return cache.addAll(contentToCache);
-//         })
-//     );
-// });
 
 self.addEventListener("install", (e) => {
     console.log("[Service Worker] Installed");
