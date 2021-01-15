bcad9820c6b53b59c5d81108f32040b4957c9a75
Piero Fusacchia <p.fusacchia@wybe.it>
2021-01-15 14:47:02 +0100

    fix

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 // Files to cache
 const cacheName = "pwaPlayground-v1";
-const appShellFiles = ["/index.html", "/main.js", "/style.css"];
+const appShellFiles = ["/pwa-playground/index.html", "/pwa-playground/main.js", "/pwa-playground/style.css"];
 // const gamesImages = [];
 // for (let i = 0; i < games.length; i++) {
 //   gamesImages.push(`data/img/${games[i].slug}.jpg`);
