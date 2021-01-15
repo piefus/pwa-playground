60c10c314a4664e8df3a8a7ffaf76948d6cfc88a
Piero Fusacchia <p.fusacchia@wybe.it>
2021-01-15 18:14:42 +0100

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // self.importScripts('data/games.js');
 
 // Files to cache
-const cacheName = "pwaPlayground-v2";
+const cacheName = ["pwaPlayground-v12"];
 const appShellFiles = ["index.html", "assets/main.js", "assets/style.css"];
 // const gamesImages = [];
 // for (let i = 0; i < games.length; i++) {
@@ -23,41 +23,84 @@ const contentToCache = appShellFiles;
 // });
 
 // Fetching content using Service Worker
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        (async () => {
-            const r = await caches.match(e.request);
-            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-            if (r) return r;
-            const response = await fetch(e.request);
-            const cache = await caches.open(cacheName);
-            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-            cache.put(e.request, response.clone());
-            return response;
-        })()
-    );
-});
+// self.addEventListener("fetch", (e) => {
+//     e.respondWith(
+//         (async () => {
+//             const r = await caches.match(e.request);
+//             console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+//             if (r) return r;
+//             const response = await fetch(e.request);
+//             const cache = await caches.open(cacheName);
+//             console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//             cache.put(e.request, response.clone());
+//             return response;
+//         })()
+//     );
+// });
 
 // contentToCache.push("/pwa-playground/style.css");
 
+// self.addEventListener("install", (e) => {
+//     e.waitUntil(
+//         caches.open("pwaPlayground-v2").then((cache) => {
+//             return cache.addAll(contentToCache);
+//         })
+//     );
+// });
+
 self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Installed");
     e.waitUntil(
-        caches.open("pwaPlayground-v2").then((cache) => {
-            return cache.addAll(contentToCache);
-        })
+        caches
+            .open(cacheName)
+            .then((cache) => {
+                console.log("[Service Worker] Caching contentToCache");
+                return cache.addAll(contentToCache);
+            })
+            .catch((err) => console.error(`[Service Worker] Caching contentToCache: ${err}`))
     );
 });
 
 self.addEventListener("activate", (e) => {
+    console.log("[Service Worker] Activated");
     e.waitUntil(
-        caches.keys().then((keyList) => {
+        caches.keys().then((cacheNames) => {
             return Promise.all(
-                keyList.map((key) => {
-                    if (key !== cacheName) {
-                        return caches.delete(key);
+                cacheNames.map((thisCacheName) => {
+                    if (!cacheName.includes(thisCacheName)) {
+                        console.log(`[Service Worker] Removing Cached Files from Cache-${thisCacheName}`);
+                        return caches.delete(thisCacheName);
                     }
                 })
             );
+        })
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    console.log(`[Service Worker] Fetching ${e.request.url}`);
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            if (response) {
+                console.log(`[Service Worker] Found in cache ${e.request.url}${response}`);
+                return response;
+            }
+
+            const requestClone = e.request.clone();
+            return fetch(requestClone)
+                .then((response) => {
+                    if (!response) {
+                        console.log("[ServiceWorker] No response from fetch ");
+                        return response;
+                    }
+                    const responseClone = response.clone();
+                    caches.open(cacheName).then((cache) => {
+                        cache.put(e.request, responseClone);
+                        console.log("[ServiceWorker] New Data Cached", e.request.url);
+                        return response;
+                    });
+                })
+                .catch((err) => console.log("[ServiceWorker] Error Fetching & Caching New Data", err));
         })
     );
 });
